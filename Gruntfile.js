678597a8f5981fad81b0be60f343ef034bd2844b
Katerina Makri <kmakri@oseven.io>
2019-07-02 18:43:17 +0300

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "css/main.css": "less/styles.less" // destination file and source file
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['less/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    },
+    concat: {
+      dist: {
+        src: [ 'less/variables.less' ],
+        dest: 'less/styles.less',
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less', 'watch', 'concat']);
+};
